5a8dd8b941c609c806fff9f969aeff4e6a51ee78
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-11-15 09:28:17 -0600

    category list is in the store

--- src/VideoStore/Categories/Categories.vb
+++ src/VideoStore/Categories/Categories.vb
@@ -1,26 +1,17 @@
-﻿Imports Microsoft.Data.SqlClient
-
-Friend Module Categories
+﻿Friend Module Categories
     Friend Sub Run(store As DataStore)
         Do
-            Dim categoryList As New List(Of (Id As Integer, Abbr As String, Name As String))
             Dim prompt As New SelectionPrompt(Of String) With {.Title = CategoriesMenu}
             prompt.AddChoice(GoBack)
             prompt.AddChoice(MenuItems.NewCategory)
             prompt.AddChoice(MenuItems.CategoryReport)
             Dim table As New Dictionary(Of String, Integer)
-            Using command = store.Connection.CreateCommand
-                command.CommandText = Commands.CategoryList
-                Using reader = command.ExecuteReader
-                    While reader.Read
-                        Dim item As (Id As Integer, Abbr As String, Name As String) = (reader.GetInt32(0), reader.GetString(1), reader.GetString(2))
-                        categoryList.Add(item)
-                        Dim fullName = $"{item.Name}({item.Abbr})#{item.Id}"
-                        prompt.AddChoice(fullName)
-                        table(fullName) = item.Id
-                    End While
-                End Using
-            End Using
+            Dim items As IEnumerable(Of (Id As Integer, Abbr As String, Name As String)) = store.CategoryList
+            For Each item In items
+                Dim fullName = $"{item.Name}({item.Abbr})#{item.Id}"
+                prompt.AddChoice(fullName)
+                table(fullName) = item.Id
+            Next
             AnsiConsole.Clear()
             Dim answer = AnsiConsole.Prompt(prompt)
             Select Case answer
--- src/VideoStore/DataStore.vb
+++ src/VideoStore/DataStore.vb
@@ -1,7 +1,23 @@
 ﻿Imports Microsoft.Data.SqlClient
+Imports Microsoft.Identity.Client
 
 Public Class DataStore
     Public Property Connection As SqlConnection
+    Public ReadOnly Property CategoryList As IEnumerable(Of (Id As Integer, Abbr As String, Name As String))
+        Get
+            Dim result As New List(Of (Id As Integer, Abbr As String, Name As String))
+            Using command = Connection.CreateCommand
+                command.CommandText = Commands.CategoryList
+                Using reader = command.ExecuteReader
+                    While reader.Read
+                        Dim item As (Id As Integer, Abbr As String, Name As String) = (reader.GetInt32(0), reader.GetString(1), reader.GetString(2))
+                        result.Add(item)
+                    End While
+                End Using
+            End Using
+            Return result
+        End Get
+    End Property
 
     Public Sub New(connection As SqlConnection)
         Me.Connection = connection
